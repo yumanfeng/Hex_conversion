498c624ad66ba9ccfa21400563746516b31ebbda
yuman.feng <yuman.feng@hotmail.com>
2015-09-17 10:59:58 +0800

    进制转换求和

--- test.js
+++ test.js
@@ -3,8 +3,10 @@
  */
 /**************
  * 字典
+ * dic=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
  **************/
-var dic = new Array();
+
+var dic = [];
 (function () {
     //0-9
     for (var i = 0; i < 10; i++) {
@@ -44,11 +46,11 @@ function tenToN(number, hex) {    //hex进制的数number
 
     if (hex != 10) {           //非10进制转换10进制
         if (parseInt(number) == number) {      //是整数
-            tenToNInt(number);
+            return tenToNInt(number);
             //alert(tenToNInterger(number));   //
         }
         else {               //是浮点数
-            var result = new Array();
+            var result = [];
             result[0] = tenToNInt(parseInt(number));              //浮点数的整数部分
             result[1] = tenToNDecimal(number - parseInt(number));      //浮点数的小数部分（以整数形式表示非小数形式）
             //alert(result.join("."));   //
@@ -63,7 +65,7 @@ function tenToN(number, hex) {    //hex进制的数number
     //整数部分的10进制转换到n进制
     function tenToNInt(number) {
         var i = 0;
-        var result = new Array();
+        var result =[];
         while (number > 0) {
             result[i] = dic[number % hex];
             number = parseInt(number / hex);   //取整数部分
@@ -75,13 +77,13 @@ function tenToN(number, hex) {    //hex进制的数number
     //小数部分的10进制转换到n进制
     function tenToNDecimal(number) {
         var i = 0;
-        var result = new Array();
+        var result = [];
         while (number != 0 && i < 10) {           //控制最多10位小数
             result[i] = dic[parseInt(number * hex)];
             number = number * hex - parseInt(number * hex);
             i++;
         }
-        for (var i = result.length - 1; result[i] == 0; i--) {       //删去小数部分尾数为0的元素
+        for (i = result.length - 1; result[i] == 0; i--) {       //删去小数部分尾数为0的元素
             result.pop();
         }
         return result.join("");
@@ -94,16 +96,15 @@ function tenToN(number, hex) {    //hex进制的数number
  **************/
 function nToTen(number, hex) {            //number为字符串
     if (hex != 10) {                //非10进制转换10进制
-        if (parseInt(number) == number) {         //是整数
-            nToTenInt(number);
-            alert(nToTenInt(number));   //
+        if (number.indexOf(".") == -1) {         //是整数
+            return nToTenInt(number);
+            //alert(nToTenInt(number));   //
         }
         else {                 //是浮点数
-            var result = new Array();
-            result = number.split(".");
+            var result = number.split(".");
             var result1 = nToTenInt(result[0]);              //浮点数的整数部分
             var result2 = nToTenDecimal(result[1]);      //浮点数的小数部分（以小数形式表示）
-            alert(result1+result2);   //
+            //alert(result1+result2);   //
             return result1 + result2;
         }
     }
@@ -135,6 +136,32 @@ function nToTen(number, hex) {            //number为字符串
         return parseFloat(result);       //去掉小数点后多余的0
         //return result;
     }
+}
+
+
+/***************
+ * 进制转换函数
+ ***************/
+function change(){
+    var number5 = document.getElementById("number5").value;
+    var hex5 = document.getElementById("hex5").value;
+    var hex4 = document.getElementById("hex4").value;
+    if(hex5 == 10){
+
+    }
+    else{
+        number5 = nToTen(number5,hex5);      //用户输入的数转换为10进制
+        if(hex4 == 10){
+            $(document).ready(function(){
+                $("#number4").val(number5);
+            });
+        }
+        else{
+
+        }
+    }
+
+
 }
 
 
